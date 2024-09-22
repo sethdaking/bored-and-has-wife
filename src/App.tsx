9be4903e7b0f34caed909846fdb8c59fb3bcf226
sethdaking <lastemail768@gmail.com>
2024-09-22 17:34:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No(Answer in brackets)",
+      "Are you sure?(Answer to the question above, No)",
+      "Are you sure you not mizing?(No)",
+      "Tell the truth(No)",
+      "With a chocolate rice cake on top(No, I'm not mizing)",
+      "Don't lie, are you ignoring me(No)",
+      "PLEASE POOKIE, tell the truth, are you ignoring me🥺(No)",
+      "Yayyy🙈💗🤭!Okay press yes, for a surprise🙈🙈🙈🙈❤(Pressing this button won't do none)"
+    
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;)) (If you got this the first time💀😑)</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">You got me so bored 😭😭? Are you ignoring me, are you talking to someone else? </h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
